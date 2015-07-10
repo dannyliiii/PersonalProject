1de624d6946b50293cfbd58433dd629ceddf7028
Danny Li <dannyleeeee@gmail.com>
2015-07-10 22:33:24 +0100

    added simple loading screen

---
+++ Assets/MyGame/Scripts/GamePlay/LoadingScreen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+	int progress = 0;
+	public GameObject text;
+	public GameObject BackgroundTexture;
+	public GameObject texture;
+	public GameObject plane;
+	// Use this for initialization
+	void Start () {
+//		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.L)) {
+			StartCoroutine(ShowLoadingScreen("KinectSample"));
+		}
+
+	}
+
+	IEnumerator ShowLoadingScreen(string scene){
+		text.SetActive (true);
+		BackgroundTexture.SetActive (true);
+		plane.SetActive (false);
+		AsyncOperation ao = Application.LoadLevelAsync (scene);
+		while (!ao.isDone) {
+			progress = (int)ao.progress * 100;
+			text.GetComponent<Text>().text = "Loading...." + progress + " %";
+			yield return null;
+		}
+	}
+}
